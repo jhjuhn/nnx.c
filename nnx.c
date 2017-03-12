83283a29399cb14097777db730391a606cb2251f
jhjuhn <jhjuhn.lok@gmail.com>
2017-03-12 01:27:28 +0000

    fix several minor bugs

--- nnx.c
+++ nnx.c
@@ -8,30 +8,22 @@ int W(int n){int r,s,t;return (t=0x1000/(r=1<<(++n)/3+(n==010))/(s=010<<n%3))|r|
 // computer plays the lower bits.
 int S(int *b){
 	int f, x;
+	int i, j;
 	// win or block win.
-	for (int i = 0; i < 16; ++i) {
-		f = w[i%8]; x = w[i%8]^w[i%8]&*b>>9;
-		if (N(f&*b>>(i>7?9:0)) == 2 && N(f&(*b|*b>>9)) == 2) {
-			puts("win.");
+	for (int k = 0; k < 16; ++k) { j = k%8;
+		f = w[j]; x = (w)[j]^(j)[w]&*b>>9;
+		if (N(f&*b>>(k>7?9:0)) == 2 && N(f&(*b|*b>>9)) == 2) {
 			return *b|= x;
 		}
 	}
 	// fork or block fork
-	int i, j;
-	for (int k = 0; k < 128; ++k) { i = (k%64)/8; j = (k%64)%8;
+	for (int k = 0; k < 128; ++k) { i = k%64/8; j = k%64%8;
 		f = w[i]|w[j]; x = w[i]&w[j];
-		if (N(f&*b>>(i>63?9:0)) == 2 && N(f&(*b|*b>>9)) == 2 && x) {
+		if (N(f&*b>>(k>63?9:0)) == 2 && N(f&(*b|*b>>9)) == 2 && x) {
 			return *b|=x;
 		}
 	}
-	/*
-	for (int k = 0; k < 64; ++k) { i = k/8; j = k%8;
-		f = w[i]|w[j]; x = w[i]&w[j];
-		if (N(f&*b>>9) == 2 && N(f&(*b|*b>>9)) == 2 && x) {
-			return *b|=x;
-		}
-	}
-	*/
+
 		
 	// centre
 	// opposite corner
@@ -54,7 +46,8 @@ printf("%c%c", b&1<<*c?88:88-(b&1<<*c+9?9:6*7), *c-*e?0:10);}}
 
 int main(void){
 	for (int i = 0; i < 8; ++i) { w[i] = W(i); }
-	int b = 2|1<<7;
+	int b = 512<<1|1<<6;
+	//int b = 0;
 	char c[2];
 	P(b);
 	while (scanf("%s", c)) {
