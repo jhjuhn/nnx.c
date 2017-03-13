25bc1db60571fac4949c5e68ad2b3cbb73dbd32a
jhjuhn <jhjuhn.lok@gmail.com>
2017-03-13 01:07:39 +0000

    minimax

--- nnx.c
+++ nnx.c
@@ -1,68 +1,82 @@
 #include <stdio.h>
 // counts # of 1 bits in r
 int N(int r){ int n; for (n=r&&1; r-=r&-r; ++n); return n; }
-int O(int b){ return b<<9; }
 // computes the ith winning position.
 int a[]={3,8,1,2,4,6,7,0,5};
 int w[8];
+
 int W(int n){int r,s,t;return (t=0x1000/(r=1<<(++n)/3+(n==010))/(s=010<<n%3))|r|s;}
-// computer plays the lower bits.
-int S(int *b){
-	int f, x;
-	int i, j;
-	// win or block win.
-	for (int k = 0; k < 16; ++k) { j = k%8;
-		f = w[j]; x = w[j]^j[w]&*b>>9;
-		if (N(f&*b>>(k>7?9:0)) == 2 && N(f&(*b|*b>>9)) == 2) {
-			return *b|= x;
-		}
-	}
-	// fork or block fork
-	for (int k = 0; k < 128; ++k) { i = k%64/8; j = k%64%8;
-		f = w[i]|w[j]; x = w[i]&w[j];
-		if (N(f&*b>>(k>63?9:0)) == 2 && N(f&(*b|*b>>9)) == 2 && x) {
-			return *b|=x;
-		}
-	}
-
-		
-	// centre
-	if (!(w[3]&w[7]&(*b|*b>>9))){
-		puts("centre");
-		return *b|=w[3]&w[7];
-	}
-	
-	// opposite corner
-	if (N(w[3]&*b>>9)==1) {
-		return *b|=w[3]^w[3]&(*b|*b>>9);
-	}
-	if (N(w[7]&*b>>9)==1) {
-		return *b|=w[7]^w[7]&(*b|*b>>9);
-	}
-
-	//first available move:
-	puts("confused.");
-	for (int k = 0; k < 18; k=++k+1){ x = 1<<a[k%9];
-		if (!(x&(*b|*b>>9))) { return *b|=x; }
-	}
-	return 0;
-}
 
 // prints the game board.
 void P(int b){ int *c, *e; for(e=(c=a)+2; c-a<9; e+=(*c++==*e)*3){
 printf("%c%c", b&1<<*c?88:88-(b&1<<*c+9?9:6*7), *c-*e?0:10);}}
 
+int M(int b, int t){
+	for (int i = 0; i < 8; ++i) {
+		if (N(b&w[i])==3) { return 1; }
+		if (N(b>>9&w[i])==3) { return -1; }
+	}
+	if (N(b)==9) { return 0; }
+
+	int r, x, m;
+	if (t == 1) {
+		r = -2; x = 0;
+		for (int j = 1; j < 1<<9; j*=2) {
+			if(j^j&(b|b>>9)) {
+				m = M(b|j, -1);
+				if (m > r) {
+					r = m;
+					x = j;
+				}
+			}
+		}
+		return r;
+	}
+
+	if (t == -1) {
+		r = 2; x = 0;
+		for (int j = 1; j < 1<<9; j*=2) {
+			if(j^j&(b|b>>9)) {
+				m = M(b|j<<9, 1);
+				if (m < r) {
+					r = m;
+					x = j<<9;
+				}
+			}
+		}
+		return r;
+	}
+}
+
+int S(int b){
+	int r = -2;
+	int x = 0;
+	int m;
+	for (int j = 1; j < 1<<9; j <<=1) {
+		if (j^j&(b|b>>9)) {
+			m = M(b|j, -1);
+			if (m > r) {
+				r = m;
+				x = j;
+			}
+		}
+	}
+	printf("R:%d X:%d\n", r, x);	
+	return b|x;
+}
+
 int main(void){
 	for (int i = 0; i < 8; ++i) { w[i] = W(i); }
-	int b = 1<<4;
+	int b = 0;
 	char c[2];
 	P(b);
 	while (scanf("%s", c)) {
 		if (('/'-*c)*(*c-'9')>0 &&
 		(1<<a[*c-'0'])&~(b|b>>9)) {
 			b |= 1<<9+a[*c-'0'];
+
 			P(b);
-			S(&b);
+			b=S(b);
 			P(b);
 		}
 	}
