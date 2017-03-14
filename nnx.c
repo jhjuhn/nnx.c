77918e9facaba404a35ba6713c327ccc5691cdf3
jhjuhn <jhjuhn.lok@gmail.com>
2017-03-14 22:38:36 +0000

    some compression

--- nnx.c
+++ nnx.c
@@ -11,16 +11,12 @@ int W(int n){int r,s,t;return (t=0x1000/(r=1<<(++n)/3+(n==010))/(s=010<<n%3))|r|
 void P(int b){ int *c, *e; for(e=(c=a)+2; c-a<9; e+=(*c++==*e)*3){
 printf("%c%c", b&1<<*c?88:88-(b&1<<*c+9?9:6*7), *c-*e?0:10);}}
 
-int M(int b, int t){
-	int u = 3<<18;
-	for (int i = 0; i < 8; ++i) {
-		if ((b&w[i])==w[i]) { return 3<<18; }
-		if ((b>>9&w[i])==w[i]) { return 1<<18; }
+int M(int b, int t){ int r, m, *k, u = 3<<18;
+	for (k = w; k < w+8; ++k) {
+		if (r=3*((b&*k)==*k)|2*((511&(b|b>>9))==511)|((b>>9&*k)==*k)) { return r<<18; }
 	}
-	if (N(b)==9) { return 2<<18; }
 
-	int r, m;
-	r = (t>0?0:4)<<18;
+	r = 2-2*t<<18;
 	for (int j = 1; j < 1<<9; j*=2) {
 		if(j^j&(b|b>>9) && t*(m=M(b|j<<(1+t?0:9),-t))-r*t>0) {
 				r = u&m|j;
