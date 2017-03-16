17f8f482d7e94bdf87e5f396fdac832ed1582c9e
jhjuhn <jhjuhn.lok@gmail.com>
2017-03-16 02:24:03 +0000

    bugs
    - rolled back
    - implement turns properly

--- nnx.c
+++ nnx.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-int w[]={0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 1, 2, 4, 6, 7, 0, 5}; int *a = w+8; int
-W(int n){int r,s,t;return (t=0x1000/(r=1<<(++n)/3+(n==010))/(s=010<<n%3))|r|s;}
+// counts # of 1 bits in r
+int N(int r){ int n; for (n=r&&1; r-=r&-r; ++n); return n; }
+// computes the ith winning position.
+int a[]={3,8,1,2,4,6,7,0,5};
+int w[8];
+
+int W(int n){int r,s,t;return (t=0x1000/(r=1<<(++n)/3+(n==010))/(s=010<<n%3))|r|s;}
+
+// prints the game board.
 void P(int b){ int *c, *e; for(e=(c=a)+2; c-a<9; e+=(*c++==*e)*3){
 printf("%c%c", b&1<<*c?88:88-(b&1<<*c+9?9:6*7), *c-*e?0:10);}}
 
 int M(int b, int t){
-	int m, j, *k, r=0;
+	int u = 3<<18;
+	for (int i = 0; i < 8; ++i) {
+		if ((b&w[i])==w[i]) { return 3<<18; }
+		if ((b>>9&w[i])==w[i]) { return 1<<18; }
+	}
+	if (N(b)==9) { return 2<<18; }
 
-	for (k = w; k < w+17; ++k) {
-
-		if (k < w + 8) {
-			r = ((m=(b&(*k=(*k?*k:W(k-w))))==*k)*3)|((b>>9&*k)==*k)|(!r&&((b|b>>9)&511)==511)*2?m<<18:r;
-		} else {
-
-			
-			//r = (!(k-w-8)&&!r?(1-t)<<19:r);
-			//if (!(k-w-8) && (r!=(1-t)<<19)) { return r; }
-			if (!(k-w-8) && (
-						(r= (!(k-w-8)&&!r?(1-t)<<19:r))
-						!=(1-t)<<19)) { return r; }
-
-			if((j=1<<*k)^j&(b|b>>9) && t*(m=M(b|j<<(1+t?0:9),-t))-r*t>0) {
-				r = 3<<18&m|j;
-			}
-
+	int r, m;
+	r = (t>0?0:4)<<18;
+	for (int j = 1; j < 1<<9; j*=2) {
+		if(j^j&(b|b>>9) && t*(m=M(b|j<<(1+t?0:9),-t))-r*t>0) {
+				r = u&m|j;
 		}
 	}
 
@@ -31,18 +31,18 @@ int M(int b, int t){
 }
 
 int main(void){
+	for (int i = 0; i < 8; ++i) { w[i] = W(i); }
 	int x = -1;
-	//for (int i = 0; i < 8; ++i) { w[i] = W(i); }
-	int b = 4*x+4;
+	int b = 4-4*x;
 	char c[2];
 	P(b);
 	while (scanf("%s", c)) {
 		if (('/'-*c)*(*c-'9')>0 &&
 		(1<<a[*c-'0'])&~(b|b>>9)) {
-			b |= 1<<a[*c-'0']+(x+1?9:0);
+			b |= 1<<a[*c-'0']+(x+1?0:9);
 
 			P(b);
-			b|=((1<<18)-1)&M(b, x)<<(x-1?9:0);
+			b|=((1<<18)-1)&M(b, -x)<<(x+1?9:0);
 			P(b);
 		}
 	}
